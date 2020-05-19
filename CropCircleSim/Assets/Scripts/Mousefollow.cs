f5a1dca667d73c0b0c2b24fbb53809fc0ef525b8
ComradeHappyPug <esther.strathy@digipen.edu>
2020-05-19 07:30:26 -0700

    uploaded unity file

---
+++ CropCircleSim/Assets/Scripts/Mousefollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mousefollow : MonoBehaviour
+{
+    Vector2 mousepos;
+    Rigidbody2D ufoRB;
+    Vector2 velocity; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mousepos = Input.mousePosition;
+        velocity.y = Mathf.MoveTowards(velocity.y, roamingspeed, 1 * Time.fixedDeltaTime);
+    }
+}
---
+++ CropCircleSim/Assets/Scripts/TinFoilHats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TinFoilHats : MonoBehaviour
+{
+    Rigidbody2D TinFoilHat;
+    Vector2 velocity;
+    public float roamingspeed = 20f;
+    public float roamingacceleration = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
