a3a5098ae88df4be3ac3347613578a14693be7b5
ComradeHappyPug <esther.strathy@digipen.edu>
2020-05-22 08:11:13 -0700

    trying to fix stuff

--- CropCircleSim/Assets/Scripts/DestroyonCollide.cs
+++ CropCircleSim/Assets/Scripts/DestroyonCollide.cs
@@ -17,6 +17,7 @@ public class DestroyonCollide : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(this.gameObject);
+        if(Input.GetMouseButton(0))
+            Destroy(this.gameObject);
     }
 }
---
+++ CropCircleSim/Assets/Scripts/accuracybar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accuracybar : MonoBehaviour
+{
+    public GameObject bar;
+    Vector3 localscale; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<Timer>().time <=0)
+        {
+            bar.transform.localScale = localscale;
+        }
+    }
+}
